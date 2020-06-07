d9e552db62e731dc7d1be1c479c67717346e0dab
Paul Glass-Steel <paul_gs@unc.edu>
2020-06-07 16:52:08 -0400

    init

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pack 845</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<style type="text/css">
+		.modal-image{
+			width: 100%;
+			height: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Pack 845 B&G Cake Contest 2020</h1>
+				<form>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_1" value="cake_1">
+					        Cake 1
+							<img
+								height="300"
+								src="photos/20200606_164150.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="20200606_164150.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/20200606_164146.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="20200606_164146.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/20200606_164050.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="20200606_164050.jpg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_2" value="cake_2">
+					        Cake 2
+							<img
+								height="300"
+								src="photos/image1.jpeg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="image1.jpeg"
+							/>
+							<img
+								height="300"
+								src="photos/image2.jpeg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="image2.jpeg"
+							/>
+							<img
+								height="300"
+								src="photos/image3.jpeg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="image3.jpeg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_3" value="cake_3">
+					        Cake 3
+							<img
+								height="300"
+								src="photos/IMG_4333.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4333.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/IMG_4334.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4334.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/IMG_4335.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4335.jpg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_4" value="cake_4">
+					        Cake 4
+							<img
+								height="300"
+								src="photos/IMG_20200607_112726.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_20200607_112726.jpg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_5" value="cake_5">
+					        Cake 5
+							<img
+								height="300"
+								src="photos/IMG_3620.JPG"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_3620.JPG"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_6" value="cake_6">
+					        Cake 6
+							<img
+								height="300"
+								src="photos/20200606_170445.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="20200606_170445.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/20200606_170451.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="20200606_170451.jpg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_7" value="cake_7">
+					        Cake 7
+							<img
+								height="300"
+								src="photos/IMG_4357.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4357.jpg"
+							/>
+					    </label>
+					</p>
+					<p>
+						<label>
+							<input type="radio" name="cake" id="cake_8" value="cake_8">
+					        Cake 8
+							<img
+								height="300"
+								src="photos/IMG_4343.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4343.jpg"
+							/>
+							<img
+								height="300"
+								src="photos/IMG_4345.jpg"
+								data-toggle="modal"
+								data-target="#exampleModal"
+								data-photo="IMG_4345.jpg"
+							/>
+					    </label>
+					</p>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      
+	      <div class="modal-body">
+	        <img class="modal-image" src=""/>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+	<script>
+		jQuery('#exampleModal').on('show.bs.modal', function (event) {
+		  var button = jQuery(event.relatedTarget); // Button that triggered the modal
+		  var src = button.data('photo'); // Extract info from data-* attributes
+		  console.log(button.data('photo'));
+		  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+		  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+		  var modal = jQuery(this);
+		  modal.find('.modal-image').attr('src','photos/' + src);
+		})
+	</script>
+</body>
+</html>
